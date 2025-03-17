0b27192d9da276f3c5ccb234ba657dcfbe9d1663
LilyLavender <lilylambda136@gmail.com>
2025-03-16 22:49:17 -0500

    Final React

---
+++ src/components/Country.jsx
@@ -0,0 +1,114 @@
+import Medal from "./Medal";
+import { Box, Table, Flex, Badge, Button, Tooltip } from "@radix-ui/themes";
+import { TrashIcon, CheckIcon, ResetIcon } from "@radix-ui/react-icons";
+
+function Country(props) {
+  function getMedalsTotal() {
+    let sum = 0;
+    props.medals.forEach((medal) => {
+      // use medal count displayed in the web page for medal count totals
+      sum += props.country[medal.name].page_value;
+    });
+    return sum;
+  }
+  // determines if there are any difference between page_value and saved_value for any medals
+  function renderSaveButton() {
+    let unsaved = false;
+    props.medals.forEach((medal) => {
+      if (
+        props.country[medal.name].page_value !==
+        props.country[medal.name].saved_value
+      ) {
+        unsaved = true;
+      }
+    });
+    return unsaved;
+  }
+
+  const glowColors = ["#FFD700", "#C0C0C0", "#CD7F32"];
+  const glowStyle = props.goldRank >= 0 ? {
+    boxShadow: `0 0 15px ${glowColors[props.goldRank]}`,
+    border: `2px solid ${glowColors[props.goldRank]}`,
+    borderRadius: "8px"
+  } : {};
+
+  return (
+    <div style={glowStyle}>
+      <Box>
+        <Table.Root variant="surface">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell colSpan="2">
+                <Flex justify="between">
+                  <span>
+                    {props.country.name}
+                    <Badge variant="outline" ml="2">
+                      {getMedalsTotal(props.country, props.medals)}
+                    </Badge>
+                  </span>
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "1.2rem",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    {renderSaveButton() && (
+                      <>
+                        <Tooltip content="Discard changes">
+                          <Button
+                            color="gray"
+                            variant="ghost"
+                            size="1"
+                            onClick={() => props.onReset(props.country.id)}
+                          >
+                            <ResetIcon />
+                          </Button>
+                        </Tooltip>
+                        <Tooltip content="Save changes">
+                          <Button
+                            color="gray"
+                            variant="ghost"
+                            size="1"
+                            onClick={() => props.onSave(props.country.id)}
+                          >
+                            <CheckIcon />
+                          </Button>
+                        </Tooltip>
+                      </>
+                    )}
+                    {props.canDelete && (
+                      <Tooltip content="Delete country">
+                        <Button color="red" variant="ghost" size="1">
+                          <TrashIcon
+                            onClick={() => props.onDelete(props.country.id)}
+                          />
+                        </Button>
+                      </Tooltip>
+                    )}
+                  </div>
+                </Flex>
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {props.medals
+              .sort((a, b) => a.rank - b.rank)
+              .map((medal) => (
+                <Medal
+                  key={medal.id}
+                  medal={medal}
+                  country={props.country}
+                  canPatch={props.canPatch}
+                  onIncrement={props.onIncrement}
+                  onDecrement={props.onDecrement}
+                />
+              ))}
+          </Table.Body>
+        </Table.Root>
+      </Box>
+    </div>
+  );
+}
+
+export default Country;
---
+++ src/components/Medal.jsx
@@ -0,0 +1,63 @@
+import { Box, Table, Flex, Badge, Button, Text, Em, Tooltip } from "@radix-ui/themes";
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import MedalSvg from "./MedalSvg";
+import { tc } from "../Utils.js";
+
+function Medal(props) {
+  return (
+    <Table.Row>
+      <Table.RowHeaderCell>
+        <Flex align="center">
+          <MedalSvg color={props.medal.color} />
+          <Box pl="2">
+            {props.country[props.medal.name].page_value !==
+            props.country[props.medal.name].saved_value ? (
+              <Text><Em>{tc(props.medal.name)} Medals</Em></Text>
+            ) : (
+              <Text>{tc(props.medal.name)} Medals</Text>
+            )}
+          </Box>
+        </Flex>
+      </Table.RowHeaderCell>
+      <Table.Cell align="right" width="108px">
+        {props.canPatch ? (
+          <Flex align="center" justify="between">
+            <Button
+              variant="ghost"
+              disabled={props.country[props.medal.name].page_value === 0}
+            >
+              <Tooltip content="Subtract medal">
+                <MinusIcon
+                  onClick={() =>
+                    props.country[props.medal.name].page_value > 0 &&
+                    props.onDecrement(props.country.id, props.medal.name)
+                  }
+                />
+              </Tooltip>
+            </Button>
+            <Badge variant="outline">
+              {props.country[props.medal.name].page_value}
+            </Badge>
+            <Button variant="ghost">
+              <Tooltip content="Add medal">
+                <PlusIcon
+                  onClick={() =>
+                    props.onIncrement(props.country.id, props.medal.name)
+                  }
+                />
+              </Tooltip>
+            </Button>
+          </Flex>
+        ) : (
+          <Flex align="center" justify="center">
+            <Badge variant="outline">
+              {props.country[props.medal.name].page_value}
+            </Badge>
+          </Flex>
+        )}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
+
+export default Medal;
